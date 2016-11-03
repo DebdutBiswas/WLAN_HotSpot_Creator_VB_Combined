cba8047c1e573fcb81c52fd6df7bf529f6a519dd
Debdut <rupam.biswas@outlook.com>
2016-11-03 19:41:27 +0530

    Some minor changes.

--- Backend_Service_App/My Project/Settings.Designer.vb
+++ Backend_Service_App/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.WLANHotspotCreator.My.MySettings
+        Friend ReadOnly Property Settings() As Global.WLANHotspotCreator.Backend.My.MySettings
             Get
-                Return Global.WLANHotspotCreator.My.MySettings.Default
+                Return Global.WLANHotspotCreator.Backend.My.MySettings.Default
             End Get
         End Property
     End Module
